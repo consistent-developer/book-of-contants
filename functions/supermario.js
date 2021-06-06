eca3a930f2a21511ee6797e49fb421d69813d0e8
Bhayu <connect.github.me@gmail.com>
2021-06-06 16:53:01 +0530

    supermario function

--- functions/supermario.js
+++ functions/supermario.js
@@ -1,5 +1,11 @@
-exports.handler =async () => {
+exports.handler = async () => {
   console.log('function ran')
 
-  const data = {name : 'mario', age: '30', }
+  const data = { name: 'mario', age: '30', job: 'plumber' }
+
+  //return responce browser
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data)
+  }
 }
