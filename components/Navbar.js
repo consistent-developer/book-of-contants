32f19a3e153db6638f49f7ba5ad0f32dc391963e
Bhayu <connect.github.me@gmail.com>
2021-06-06 15:24:27 +0530

    login & logout

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,26 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { useContext } from 'react'
+import AuthContext from '../stores/authContaxts'
 
-export default function Navbar() {
+export default function Navbar () {
+
+  const {user, login, logout} = useContext(AuthContext)
+  console.log(user)
   return (
-    <div className="container">
+    < div className="container" >
       <nav>
         <Image src="/rupee.png" width={50} height={48} />
         <h1>Gaming Vibes</h1>
         <ul>
           <li><Link href="/"><a>Home</a></Link></li>
           <li><Link href="/guides"><a>Guides</a></Link></li>
+          <li onClick = {login} className = "btn">Log in/Sign up</li>
+          <li onClick = {logout} className = "btn">Log Out</li>
         </ul>
       </nav>
       <div className="banner">
-        <Image src="/banner.png" width={966} height={276} layout="responsive"/>
+        <Image src="/banner.png" width={966} height={276} layout="responsive" />
       </div>
     </div>
   )
--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,16 @@
 import Navbar from '../components/Navbar'
+import { AuthContextProvider } from '../stores/authContaxts'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return (
     <div>
-      <Navbar />
-      <Component {...pageProps} />
+
+      <AuthContextProvider>
+        <Navbar />
+        <Component {...pageProps} />
+      </AuthContextProvider>
+      
     </div>
   )
 }
