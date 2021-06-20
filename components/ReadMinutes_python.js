4dc25b5a58eaa5fd13ec6c5807964ba62f51d22d
Bhayu <connect.github.me@gmail.com>
2021-06-20 19:37:46 +0530

    main menu

--- components/ReadMinutes_python.js
+++ components/ReadMinutes_python.js
@@ -1,9 +1,15 @@
+import Link from "next/link";
+
 const ReadMinutes = () => {
   return (
     <div>
 
       <div className="mt-6 grid grid-cols-2 text2 px-1 lg:px-20">
-        <h6 className="w-20 md:w-28 p-0.5 text-sm lg:text-base text-center self-center text-white bg-green-400">Python</h6>
+        <Link href="/python" >
+          <a>
+            <h6 className="w-20 md:w-28 p-0.5 text-sm lg:text-base text-center self-center text-white bg-green-400">Python</h6>
+          </a>
+        </Link>
         <div className="flex place-self-end justify-items-center bg-green-400 px-1.5 py-0.5 md:px-2 text-white">
           <i className="far fa-clock mr-1 text-base text-green-900 flex-auto self-center"></i>
           <h6 className="flex-auto self-center text-sm lg:text-base ml-1">10 minute</h6>
